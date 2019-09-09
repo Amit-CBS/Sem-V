2c42d824259cc0bcc7fb55a6bef9eb94deee8f5e
Amit-CBS <cbs.amitrajsingh@gmail.com>
2019-09-09 20:01:58 +0530

    Arranged

--- DAA/Doubts/Try.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("Do or do not, there is no TRY.\n");
-}
--- Java/Doubts/Try.java
+++
@@ -1,8 +0,0 @@
-class Try
-{
-    public static void main(String args[])
-    {
-        String s="Do or do not, there is no TRY.";
-        System.out.println(s);
-    }
-}
